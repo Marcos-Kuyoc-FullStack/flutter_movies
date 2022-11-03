25d7324debed5f69e32872023e223fc50b84cd05
Marcos Kuyoc <marcos.kuyoc@gulf.mx>
2022-11-03 09:50:14 -0600

    feat: se crea el proyecto para la aplicacion de peliculas con 2 screens principales

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:fl_movies/screens/screens.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Peliculas',
+      initialRoute: 'home',
+      routes: {
+        'home': (_) => const HomeScreen(),
+        'details': (_) => const DetailsScreen(),
+      },
+      theme: ThemeData.light().copyWith(
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          color: Colors.indigo,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/screens.dart
@@ -0,0 +1,2 @@
+export 'package:fl_movies/screens/details.dart';
+export 'package:fl_movies/screens/home.dart';
