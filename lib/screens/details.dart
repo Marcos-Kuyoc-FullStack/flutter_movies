4792f79e32f69cdb4e1d940c69ce798b1d6d1c46
Marcos Kuyoc <marcos.kuyoc@gulf.mx>
2022-11-10 10:51:06 -0600

    feat: se agrega efecto entre componentes de heroId para que se vea bonito

--- lib/screens/details.dart
+++ lib/screens/details.dart
@@ -80,14 +80,17 @@ class _PosterAndTitle extends StatelessWidget {
       margin: const EdgeInsets.only(top: 20),
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Row(children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(20),
-          child: FadeInImage(
-            placeholder: const AssetImage('assets/no-image.jpg'),
-            image: NetworkImage(movie.fullPosterImg),
-            height: 150,
-            width: 110,
-            fit: BoxFit.cover,
+        Hero(
+          tag: movie.heroId!,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: FadeInImage(
+              placeholder: const AssetImage('assets/no-image.jpg'),
+              image: NetworkImage(movie.fullPosterImg),
+              height: 150,
+              width: 110,
+              fit: BoxFit.cover,
+            ),
           ),
         ),
         const SizedBox(width: 20),
--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -62,6 +62,7 @@ class _MovieSliderState extends State<MovieSlider> {
               itemCount: widget.movies.length,
               itemBuilder: (_, int index) => _MoviePoster(
                 movie: widget.movies[index],
+                heroId: '${widget.title}-$index-${widget.movies[index].id}',
               ),
             ),
           ),
@@ -73,14 +74,18 @@ class _MovieSliderState extends State<MovieSlider> {
 
 class _MoviePoster extends StatelessWidget {
   final Movie movie;
+  final String heroId;
 
   const _MoviePoster({
     Key? key,
     required this.movie,
+    required this.heroId,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    movie.heroId = heroId;
+
     return Container(
       width: 130,
       height: 190,
@@ -95,14 +100,17 @@ class _MoviePoster extends StatelessWidget {
               'details',
               arguments: movie,
             ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: FadeInImage(
-                placeholder: const AssetImage('assets/no-image.jpg'),
-                image: NetworkImage(movie.fullPosterImg),
-                width: 130,
-                height: 190,
-                fit: BoxFit.cover,
+            child: Hero(
+              tag: movie.heroId!,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: FadeInImage(
+                  placeholder: const AssetImage('assets/no-image.jpg'),
+                  image: NetworkImage(movie.fullPosterImg),
+                  width: 130,
+                  height: 190,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
           ),
